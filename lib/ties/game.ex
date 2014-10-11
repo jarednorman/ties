03b9f39bd61d59887107b3dcfec6c517a9e84aad
Jared Norman <jared@freerunningtechnologies.com>
2014-10-11 00:14:00 -0700

    Ask players for their moves
    Change-Id: I2df8da8603cb2ba2dc092d8ee82eee9fe2bf664b

--- lib/ties/game.ex
+++ lib/ties/game.ex
@@ -1,4 +1,5 @@
 defmodule Ties.Game do
+  @derive [Access]
   defstruct [x: nil, o: nil, game_state: nil]
 
   alias Ties.Game
@@ -20,11 +21,36 @@ defmodule Ties.Game do
   end
 
   def do_turn(game, current_player, other_player) do
+    player = game[current_player]
+    tell player, "It is your turn.\n"
+    tell player, format_board(game)
+    tell player, "Where would you like to play? "
+    turn = get_turn(player)
     do_turn(game, other_player, current_player)
   end
 
-  def notify_players(%Game{x: x_player, o: o_player}) do
-    :gen_tcp.send x_player, "You are X.\n"
-    :gen_tcp.send o_player, "You are O.\n"
+  def notify_players(game = %Game{x: x_player, o: o_player}) do
+    tell x_player, "You are X.\n"
+    tell o_player, "You are O.\n"
+    game
   end
+
+  defp tell(player, message) do
+    :gen_tcp.send(player, message)
+    player
+  end
+
+  defp listen(player) do
+    {:ok, message} = :gen_tcp.recv(player, 0)
+    message
+  end
+
+  defp get_turn(player) do
+    player
+    |> listen
+    |> String.rstrip
+    |> String.to_integer
+  end
+
+  defp format_board(game), do: "yolo\n"
 end
